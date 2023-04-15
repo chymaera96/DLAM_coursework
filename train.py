1796724c6efea6da8f654aaf32d94e2268d5c54f
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-04-15 04:52:29 +0100

    assignment error fix

--- train.py
+++ train.py
@@ -97,10 +97,10 @@ def main():
     sample_rate = args.sr
     random_seed = 42
 
-    data_dir = create_train_set(data_dir)
-    assert data_dir == os.path.join(root,"data/fma_8000") and len(os.listdir(data_dir)) == int(data_dir.split('_')[-1])
+    sub_dir = create_train_set(data_dir)
+    assert sub_dir == os.path.join(root,"data/fma_8000") and len(os.listdir(sub_dir)) == int(sub_dir.split('_')[-1])
 
-    train_dataset = NeuralfpDataset(path=data_dir, transform=TransformNeuralfp(ir_dir=ir_dir, noise_dir=noise_dir,sample_rate=sample_rate), train=True)
+    train_dataset = NeuralfpDataset(path=sub_dir, transform=TransformNeuralfp(ir_dir=ir_dir, noise_dir=noise_dir,sample_rate=sample_rate), train=True)
     # validation_dataset = NeuralfpDataset(path=data_dir, transform=TransformNeuralfp(ir_dir=ir_dir, noise_dir=noise_dir,sample_rate=sample_rate), train=True)
 
     # dataset_size = len(train_dataset)
