269ff99bf07fd21ae3deb9a957287a30dc211501
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-04-18 22:39:52 +0100

    print hit rates

--- eval.py
+++ eval.py
@@ -297,6 +297,7 @@ def eval_faiss(emb_dir,
     # top1_song = 100 * np.mean(top1_song[:ti + 1, :], axis=0)
 
     hit_rates = np.concatenate([top1_exact_rate, top1_near_rate, top3_exact_rate, top10_exact_rate])
+    print(hit_rates)
 
     del fake_recon_index, query, db
     np.save(f'{emb_dir}/raw_score.npy',
