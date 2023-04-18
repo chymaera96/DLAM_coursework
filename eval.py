7a640982133765ede5f0b8120833a68f2447269d
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-04-18 22:51:47 +0100

    formatting hit rates

--- eval.py
+++ eval.py
@@ -291,14 +291,13 @@ def eval_faiss(emb_dir,
 
     # Summary
     top1_exact_rate = 100. * np.mean(top1_exact, axis=0)
-    print(top1_exact_rate )
     top1_near_rate = 100. * np.mean(top1_near, axis=0)
     top3_exact_rate = 100. * np.mean(top3_exact, axis=0)
     top10_exact_rate = 100. * np.mean(top10_exact, axis=0)
     # top1_song = 100 * np.mean(top1_song[:ti + 1, :], axis=0)
 
-    hit_rates = np.concatenate([top1_exact_rate, top1_near_rate, top3_exact_rate, top10_exact_rate])
-    print(hit_rates)
+    hit_rates = np.stack([top1_exact_rate, top1_near_rate, top3_exact_rate, top10_exact_rate])
+    # print(hit_rates)
 
     del fake_recon_index, query, db
     np.save(f'{emb_dir}/raw_score.npy',
