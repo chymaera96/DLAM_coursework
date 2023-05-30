6a3576632781ed577ca6916df1cef12738daa564
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2023-05-30 18:12:49 +0100

    test larger batch size

--- train.py
+++ train.py
@@ -122,7 +122,7 @@ def main():
     noise_dir = args.noise_dir
     
     # Hyperparameters
-    batch_size = 64
+    batch_size = 120
     learning_rate = 1e-4
     num_epochs = args.epochs
     sample_rate = args.sr
